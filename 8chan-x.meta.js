dd9121ee1082f3148cd62264c6f221d86f09a51a
Gen2 <invisibro@cock.li>
2017-05-08 07:36:28 -0400

    Delete 8chan-x.meta.js
    Serves no purpose

--- 8chan-x.meta.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name        Pashe's 8chanX v2 [pure]
-// @version     2.0.0.1457850720
-// @description Small userscript to improve 8chan
-// @namespace   https://github.com/Pashe/8chanX/tree/2-0
-// @updateURL   https://github.com/Pashe/8chanX/raw/2-0/8chan-x.meta.js
-// @downloadURL https://github.com/Pashe/8chanX/raw/2-0/8chan-x.user.js
-// ==/UserScript==
